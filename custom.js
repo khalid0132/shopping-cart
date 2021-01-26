bcafdab90908f19627b2475a9a600199fe127f2b
Khalid Saifullah <khalid0132@gmail.com>
2021-01-26 21:53:41 +0100

    all buttons work but no function added

--- custom.js
+++ custom.js
@@ -12,19 +12,123 @@ plusButton.addEventListener('click', function(){
     const presentPrice = parseFloat(currentPrice);
     const addedPrice = presentPrice + 1219;
     document.getElementById('currentPrice').innerText = addedPrice;
+    // pricing = document.getElementById('currentPrice').innerText;
+
+    // subtotal part 
+    const subtotal = document.getElementById('subtotal').innerText;
+    const subtotalPrice= parseFloat(subtotal);
+    const subtotalCost = subtotalPrice + 1219;
+    document.getElementById('subtotal').innerText = subtotalCost;
+
+    //Tax part
+
+
+    // Total part // Total part (( We have to use a function inside))
+    const total = document.getElementById('total').innerText;
+    const totalPrice= parseFloat(total);
+    const totalCost = totalPrice + 1219;
+    document.getElementById('total').innerText = totalCost;
+
 });
 
-// minusButton works here
+
+
+
+
+// minusButton works here------------------------
 const minusButton = document.getElementById('minusButton');
 minusButton.addEventListener('click', function(){
+    
     const currentInput = document.getElementById('currentInput').value;
     const currentItem = parseFloat(currentInput);
-    const minusItem = currentItem - 1;
-    document.getElementById('currentInput').value = minusItem;
-
+        const minusItem = currentItem - 1;
+        document.getElementById('currentInput').value = minusItem;    
+  
     // current price department here
     const currentPrice = document.getElementById('currentPrice').innerText;
     const presentPrice = parseFloat(currentPrice);
     const minusPrice = presentPrice - 1219;
     document.getElementById('currentPrice').innerText = minusPrice;
+
+       // subtotal part
+       const subtotal = document.getElementById('subtotal').innerText;
+       const subtotalPrice= parseFloat(subtotal);
+       const subtotalCost = subtotalPrice - 1219;
+       document.getElementById('subtotal').innerText = subtotalCost;
+   
+       //Tax part
+       
+   
+       // Total part (( We have to use a function inside))
+       const total = document.getElementById('total').innerText;
+       const totalPrice= parseFloat(total);
+       const totalCost = totalPrice - 1219;
+       document.getElementById('total').innerText = totalCost;
+
+   
 });
+
+// Casing department--------------
+// plusButton works here
+const plusButtonCase = document.getElementById('plusButtonCase');
+plusButtonCase.addEventListener('click', function(){
+    const currentInputCase = document.getElementById('currentInputCase').value;
+    const currentItemCase = parseFloat(currentInputCase);
+    const addedItemCase = currentItemCase + 1;
+    document.getElementById('currentInputCase').value = addedItemCase;
+
+    // current price department here
+    const currentPriceCase = document.getElementById('currentPriceCase').innerText;
+    const presentPriceCase = parseFloat(currentPriceCase);
+    const addedPriceCase = presentPriceCase + 59;
+    document.getElementById('currentPriceCase').innerText = addedPriceCase;
+
+        // // subtotal part 
+        const subtotal = document.getElementById('subtotal').innerText;
+        const subtotalPrice= parseFloat(subtotal);
+        const subtotalCost = subtotalPrice + 59;
+        document.getElementById('subtotal').innerText = subtotalCost;
+    
+        // //Tax part
+    
+    
+        // // Total part // Total part (( We have to use a function inside))
+        const total = document.getElementById('total').innerText;
+        const totalPrice= parseFloat(total);
+        const totalCost = totalPrice + 59;
+        document.getElementById('total').innerText = totalCost;
+
+});
+
+        // minusButtonCase department
+        const minusButtonCase = document.getElementById('minusButtonCase');
+        minusButtonCase.addEventListener('click', function(){
+           
+            const currentInputCase = document.getElementById('currentInputCase').value;
+           const currentItemCase= parseFloat(currentInputCase);
+           const minusItemCase = currentItemCase - 1;
+           console.log(minusItemCase);
+           document.getElementById('currentInputCase').value = minusItemCase;
+
+        // currentPriceCase starts
+            const currentPriceCase = document.getElementById('currentPriceCase').innerText;
+            const presentPriceCase = parseFloat(currentPriceCase);
+            const minusPriceCase = presentPriceCase - 59;   
+            document.getElementById('currentPriceCase').innerText = minusPriceCase;
+        
+                // // subtotal part 
+                const subtotal = document.getElementById('subtotal').innerText;
+                const subtotalPrice= parseFloat(subtotal);
+                const subtotalCost = subtotalPrice - 59;
+                document.getElementById('subtotal').innerText = subtotalCost;
+            
+                // //Tax part
+            
+            
+                // // Total part // Total part (( We have to use a function inside))
+                const total = document.getElementById('total').innerText;
+                const totalPrice= parseFloat(total);
+                const totalCost = totalPrice - 59;
+                document.getElementById('total').innerText = totalCost;
+        
+        });
